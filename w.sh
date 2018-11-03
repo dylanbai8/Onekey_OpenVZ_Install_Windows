3ce5bc52c5b0f572a0c3f049b952080b91bc48ba
yude268@gmail.com <yude268>
2018-11-03 21:04:33 +0800

    Signed-off-by: yude268@gmail.com <yude268>

--- w.sh
+++ w.sh
@@ -134,6 +134,7 @@ start_vnc(){
 vncserver -kill :1
 lsof -i:"3389" | awk '{print $2}'| grep -v "PID" | xargs kill -9
 vncserver :1
+qemu-system-x86_64 -hda /root/IMG/win.img -m 512M -smp 1 -daemonize -nographic -vnc :2 -net nic -net user -redir tcp:3389::3389
 
 local_ip=`curl -4 ip.sb`
 clear
